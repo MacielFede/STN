d67b89d31b167966665b2545ec9d32772332e224
Shifter <felipenahuelviera@hotmail.com>
2025-05-24 22:52:05 -0300

    Revert "backend/bus_line_schedule (#10)"
    This reverts commit 71773f7c6283c34699e2ab204527315357b99269.

--- backend/src/main/java/com/tecnoinf/tsig/stn/repository/BusLineScheduleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tecnoinf.tsig.stn.repository;
-
-import com.tecnoinf.tsig.stn.model.BusLineSchedule;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BusLineScheduleRepository extends JpaRepository<BusLineSchedule, Long> {
-}
