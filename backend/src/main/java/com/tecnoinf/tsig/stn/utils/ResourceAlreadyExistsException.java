94f4edd27db6dd82d5979d3cc6dbb67e236ee9ae
Nitram13E <martinquot@gmail.com>
2025-06-22 04:39:00 -0300

    Se realiza corrección sobre el control de errores de login para permitir retornar errores de tipo Bad Request e Internal Server Error entre otros redefiniendo el GlobalExceptionHandler. También se realizan modificaciones sobre los controllers de BusLine y StopLine para agregar validación de RequestBody.

--- backend/src/main/java/com/tecnoinf/tsig/stn/utils/ResourceAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.tecnoinf.tsig.stn.utils;
-
-public class ResourceAlreadyExistsException extends RuntimeException {
-    public ResourceAlreadyExistsException(String message) {
-        super(message);
-    }
-}
