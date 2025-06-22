49d9e29f01bb92a3b7ff67c14e2629fcf856a33f
Nitram13E <53955428+Nitram13E@users.noreply.github.com>
2025-06-22 05:00:53 -0300

    feature(backend/init-data): Implement initial data loading container and scripts for Geoserver with idempotent execution (#34)
    * Revert "Frontend/paradas default inactivo (#33)"
    
    This reverts commit 5677681af3af5d953aa3e829b3da712a6aa1c749.
    
    * Se realiza la implementación del container y scripts requeridos para la carga de datos iniciales particulares para Geoserver y datos de prueba. Se define de forma tal que se ejecuten los scripts de init/data según el orden definido, por lo que en caso de requerir mas información unicamente se debe agregar los scripts a dicho directorio. La carga se realiza en paralelo con los otros containers y los scripts se formularo para mantener la integridad de los datos, unicamente creando e insertando información si esta no existe.

--- frontend/src/components/molecules/admin/BusStopForm.tsx
+++ frontend/src/components/molecules/admin/BusStopForm.tsx
@@ -43,10 +43,6 @@ const BusStopForm = ({ stop, setStop, resetActiveStop }: BusStopFormProps) => {
         }
         await createStop({
           ...data,
-          geometry:{
-            type: 'Point',
-            coordinates: [data.geometry.coordinates[1], data.geometry.coordinates[0]]
-          },
           department: turnCapitalizedDepartment(
             stopContext.properties.department,
           ) as Department,
